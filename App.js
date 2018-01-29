27371a75e6101b88c6a6f74b45a5130e6a8720ac
Mykyta Shytik <mykyta.shytik@gmail.com>
2018-01-29 19:28:37 +0200

    task2

--- App.js
+++ App.js
@@ -8,23 +8,16 @@ export default class App extends React.Component {
 
 	constructor(props) {
 		super(props)
-		this.authView = (<Auth />);
+		this.authView = (<Auth />)
 		this.transactionsView = (<Transactions />)
 		this.state = {token: ""}
-
-		this.authView.onAuth = (receivedToken) => { this.setState(token: receivedToken) }
 	}
 
 	render() {
-
-		console.log(this.state.token)
-		console.log("lol")
 		return (
 			<View style={{flex: 1}}>
 				<StatusBar barStyle='light-content'/>
-				{ 
-					(this.state.token.length > 0) ? this.transactionsView : this.authView
-			 	}
+				{(this.state.token.length > 0) ? this.transactionsView : this.authView}
 			</View>
 		);
 	}
